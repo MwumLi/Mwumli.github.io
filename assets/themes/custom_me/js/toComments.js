4a182f81a72331d0d1e1c124f89b75e0b2a58a4a
mwumli <mwumli@hotmail.com>
2015-08-20 05:12:10 +0000

    七夕,放在心底，才会沉淀

--- assets/themes/custom_me/js/toComments.js
+++ assets/themes/custom_me/js/toComments.js
@@ -1,4 +1,8 @@
 
+function scrollToElement(elementStr){
+	var targetOffset = $(elementStr).offset().top;
+	$("html,body").animate({scrollTop: targetOffset}, 400);
+}
 function toComments() {
 
 	$(".nav-bottom")
@@ -7,37 +11,28 @@ function toComments() {
 			$(this).stop().fadeTo(300, '0.9');
 		}, function(){
 		$(this).stop().fadeTo(300, "0.25");														});
-	
-	// set scroll to top
-	$('#site-top').click(function(){
-		var targetOffset = $("#header").offset().top;
-		$("html,body").animate({scrollTop: targetOffset}, 400);
-
-	});
-
+		
 	if($("#to_comments").length > 0) {
 		$("#site-bottom").hide();
 		$("#site-comment").css("border-bottom","none");
 	} else {
 		$("#site-comment").hide()
 	}
+
+	// set scroll to top
+	$('#site-top').click(function(){
+		scrollToElement("#header");
+	});
+
 	// set scroll to bottom
 	$('#site-bottom').click(function(){
-		var targetOffset = $("#footer").offset().top;
-		$("html,body").animate({scrollTop: targetOffset}, 400);
-
+		scrollToElement("#footer");
 	});
 
+	// set scroll to comment
 	$("#site-comment").click(function(){
-		var targetOffset = $("#to_comments").offset().top;
-		$("html,body").animate({scrollTop: targetOffset}, 400);
+		scrollToElement("#to_comments");
 	});
 
-	$("nav-bottom-comment").click(function(){
-		var e = $(this).children("a");
-		var target = document.getElementById(e.attr("href").slice(1));
-		var targetOffset = $(target).offset().top;
-		$("html,body").animate({scrollTop: targetOffset}, 400);
-	});
 		
 }
